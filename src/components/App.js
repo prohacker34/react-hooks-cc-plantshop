1d4f2c86e60773d3c670e5d3b1fd5649c65c4d26
Habert <habertotieno4@gmail.com>
2025-04-25 14:46:09 +0300

    plantsy

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,20 @@
-import React from "react";
+
 import Header from "./Header";
 import PlantPage from "./PlantPage";
-
+import { useState,useEffect } from "react";
 function App() {
+  const [plants, setPlants] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((r) => r.json())
+      .then((plants) => setPlants(plants));
+  }
+  , []);
   return (
     <div className="app">
       <Header />
-      <PlantPage />
+      <PlantPage plants={plants}/>
     </div>
   );
 }
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((r) => r.json())
+      .then((plants) => setPlants(plants));
+  }, []);
+
+  function handleAddPlant(newPlant) {
+    setPlants([...plants, newPlant]);
+  }
+
+  const displayedPlants = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={handleAddPlant} />
+      <Search onSearch={setSearchTerm} />
+      <PlantList plants={displayedPlants} />
     </main>
   );
 }
